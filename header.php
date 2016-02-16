24ae62f287cd05f162832ffd3cd455d0721ac39e
lilis2nd <lilis@skyremix.com>
2016-02-16 20:35:07 +0900

    Framework change
    Changed from Bootstrap to UIKIT

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="./css/bootstrap.min.css">
-	<link rel="stylesheet" href="./css/qc.css">
+	<link rel="stylesheet" href="./css/uikit.min.css" />
+	<link rel="stylesheet" href="./css/qc.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/uikit.min.js"></script>
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
