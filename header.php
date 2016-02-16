9b52723a24d92efa99a95d407e4b40970a3371c1
lilis2nd <lilis@skyremix.com>
2016-02-16 20:09:18 +0900

    file separation
    index / header / main / footer

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="ko">
 <!-- Header -->
 <head>
-	<title></title>
+	<title>QC Quality Checklist</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,6 +14,5 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php include('header.php'); ?>
-
-
-<?php include('footer.php'); ?>
+<?php
+include('header.php');
+include('main.php');
+include('footer.php');
+?>
