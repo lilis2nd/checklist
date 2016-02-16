f0d81be254ab73221c7767b9d9ee436344d67ded
lilis2nd <lilis@skyremix.com>
2016-02-16 21:23:22 +0900

    Update main.php

--- main.php
+++ main.php
@@ -3,13 +3,13 @@
 
 <main>
 	<div class="uk-grid">
-	  <div class="uk-width-5-6 uk-container-center">
-	    <form class="uk-form">
-	      <fieldset data-uk-margin>
-	        <legend>Basic information</legend>
-	        <input type="text" placeholder="모델명" />
-	      </fieldset>
-	    </form>
-	  </div>
+		<div class="uk-width-5-6 uk-container-center">
+			<form class="uk-form">
+				<fieldset data-uk-margin>
+					<legend>Basic information</legend>
+					<input type="text" placeholder="모델명" />
+				</fieldset>
+			</form>
+		</div>
 	</div>
 </main>
