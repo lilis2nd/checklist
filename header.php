5a52d19b3241f2d11340c771b90428cbfcb77fbb
lilis2nd <lilis@skyremix.com>
2016-02-23 17:18:17 +0900

    메인 작업

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 		<title>QC Quality Checklist</title>
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/qc.css" rel="stylesheet">
+		<link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
--- main.php
+++ main.php
@@ -1,5 +1,78 @@
 <header>
+	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+				<a class="navbar-brand"><b>Quality Checklist</b></a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="menu">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="./">품질검수</a></li>
+					<li><a href="#">검수이력</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 </header>
 
 <main>
+	<div class="container">
+		<form class="form-horizontal">
+			<fieldset>
+				<legend>Basic information</legend>
+				<div class="form-group form-group-sm">
+					<!-- 모델명 -->
+					<label for="model" class="col-sm-1 control-label">모델명</label>
+					<div class="col-sm-3">
+						<input type="text" class="form-control" id="model" name="model" placeholder="모델명" />
+					</div>
+					<!-- 자재 -->
+					<label for="type" class="col-sm-1 control-label">자재</label>
+					<div class="col-sm-3">
+						<select name="type" id="type" class="form-control">
+							<option>QSG</option>
+							<option>UM</option>
+						</select>
+					</div>
+					<!-- 출향지 -->
+					<label for="dest" class="col-sm-1 control-label">출향지</label>
+					<div class="col-sm-3">
+						<select name="dest" id="dest" class="form-control">
+							<option>EU</option>
+							<option>CIS</option>
+						</select>
+					</div>
+				</div>
+				<div class="form-group form-group-sm">
+					<!-- 언어 -->
+					<label for="language" class="col-sm-1 control-label">언어</label>
+					<div class="col-sm-3">
+						<select name="language" id="language" class="form-control">
+							<option>English</option>
+						</select>
+					</div>
+					<!-- 검수자 -->
+					<label for="person" class="col-sm-1 control-label">검수자</label>
+					<div class="col-sm-3">
+						<input type="text" name="person" id="person" class="form-control" />
+					</div>
+					<!-- 검수일 -->
+					<label for="date" class="col-sm-1 control-label">검수일</label>
+					<div class="col-sm-3">
+						<input type="date" name="date" id="date" class="form-control" />
+					</div>
+				</div>
+			</fieldset>
+
+			<fieldset>
+				<legend>세부 항목</legend>
+			</fieldset>
+		</form>
+	</div>
 </main>
