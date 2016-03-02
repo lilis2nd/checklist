d6c581cf1ec97a99ccd83a14f4f9fb91a5e976c8
lilis2nd <lilis@skyremix.com>
2016-03-02 11:01:17 +0900

    세부 수정

--- main.php
+++ main.php
@@ -1,6 +1,12 @@
 <?php
 
+// Session restart
 session_start();
+session_unset();
+session_destroy();
+session_write_close();
+setcookie(session_name(),'',0,'/');
+session_regenerate_id(true);
 
 $type = ['QSG', 'UM'];
 $os = ['Jellybean', 'Kitkat', 'Lollipop', 'Marshmallow'];
