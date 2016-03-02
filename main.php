3eca78a39f83802c7b56f5187e123b65a53f16e7
lilis2nd <lilis@skyremix.com>
2016-03-02 10:35:29 +0900

    this is SPARTA!
    arrgh!

--- main.php
+++ main.php
@@ -91,37 +91,6 @@ function selector($array) {
 </header>
 
 <main>
-<<<<<<< HEAD
-  <div class="uk-grid">
-    <div class="uk-width-1-6 uk-push-4-6">
-      <div class="uk-panel">
-        <h3 class="uk-panel-title">haha</h3>
-      </div>
-    </div>
-    <div class="uk-width-3-6">
-      <form class="uk-form uk-form-horizontal">
-        <fieldset data-uk-margin>
-          <legend>Basic information</legend>
-          <div class="uk-form-row">
-            <label for="model" class="uk-form-label uk-form-width-small">모델명</label>
-            <div class="uk-form-controls">
-              <input class="uk-form-small uk-form-width-medium" type="text" name="model" placeholder="모델명" />
-            </div>
-          </div>
-          <div class="uk-form-row">
-            <label for="dest" class="uk-form-label uk-form-width-small">출향지</label>
-            <div class="uk-form-controls">
-              <select class="uk-form-small uk-form-width-medium" name="dest">
-                <option>EU</option>
-                <option>CIS</option>
-              </select>
-            </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-  </div>
-=======
 	<div class="container">
 		<form class="form-horizontal">
 			<fieldset>
@@ -224,5 +193,4 @@ function selector($array) {
 			</fieldset>
 		</form>
 	</div>
->>>>>>> master
 </main>
