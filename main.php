6843558c2da51d030522d090ce91c050ee6a215f
lilis2nd <lilis@skyremix.com>
2016-03-02 11:10:07 +0900

    11
    11

--- main.php
+++ main.php
@@ -6,7 +6,7 @@ session_unset();
 session_destroy();
 session_write_close();
 setcookie(session_name(),'',0,'/');
-session_regenerate_id(true);
+session_regenerate_id();
 
 $type = ['QSG', 'UM'];
 $os = ['Jellybean', 'Kitkat', 'Lollipop', 'Marshmallow'];
